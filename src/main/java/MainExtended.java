be8754fe713a8a954ad9abe829c52c68a251e683
Mateusz Filip <starszy1994@gmail.com>
2022-01-04 19:16:13 +0100

    Second version

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {         // [2]
+        String mySurname = "Filip";
+        int favouritNumber = 6;
+        double myWeight = 75.50;
+        char energyClass = 'A';
+        boolean isWoman = false;
+        System.out.println(mySurname);
+        System.out.println(favouritNumber);
+        System.out.println(myWeight);
+        System.out.println(energyClass);
+        System.out.println(isWoman);
+
+    }
+}
