90d63628ba6d8603b3d5ad5b9f62be9fd22ba224
Mateusz Filip <starszy1994@gmail.com>
2022-01-04 18:48:27 +0100

    VersionOne

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] agrs) {
+        System.out.println("Hello World!");
+        System.out.println("Mateusz");
+        System.out.println(27);
+        System.out.println('F');
+    }
+}
