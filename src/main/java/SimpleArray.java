e23d8035dac6038a380b6e5904c09cd7b93a2408
Mateusz Filip <starszy1994@gmail.com>
2022-01-30 14:21:04 +0100

    New Array

---
+++ src/main/java/SimpleArray.java
@@ -0,0 +1,25 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        ;
+
+        String[] sports = new String[5];
+
+        sports[0] = "Football";
+        sports[1] = "Volleyball";
+        sports[2] = "Tennis";
+        sports[3] = "Swimming";
+        sports[4] = "Running";
+
+        String sport = sports[3];
+        {
+            System.out.println(sports[3]);
+        }
+        int numerOfElements = sports.length;
+
+        if (numerOfElements < 6){
+            System.out.println("Moja tablica zawiera 5 elementów");
+        } else {
+            System.out.println("Moja tablica nie zawiera 5 elementów");
+        }
+    }
+}
