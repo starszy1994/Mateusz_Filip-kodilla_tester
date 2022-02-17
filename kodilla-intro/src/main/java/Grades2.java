938bb7d453774b18787046de5605d283cd2901b3
Mateusz Filip <starszy1994@gmail.com>
2022-02-17 11:30:48 +0100

    oceny

--- kodilla-intro/src/main/java/Grades2.java
+++ kodilla-intro/src/main/java/Grades2.java
@@ -2,7 +2,7 @@ public class Grades2 {
     public static void main(String[] agrs) {
     Grades grades = new Grades();
     grades.add(2);
-    grades.add(5);
+    grades.add(4);
     int lastGrades=grades.returnLastGrade();
     System.out.println(lastGrades);
     double returnAverage=grades.returnAverage();
