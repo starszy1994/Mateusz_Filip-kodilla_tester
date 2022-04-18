92fdc9cd072f098d5f845ad4d5323d89afa3d64c
Mateusz Filip <starszy1994@gmail.com>
2022-04-18 13:42:22 +0200

    bean test

--- kodilla-spring-basic/src/main/java/spring/basic/SpringBasicRunner.java
+++ kodilla-spring-basic/src/main/java/spring/basic/SpringBasicRunner.java
@@ -1,5 +1,8 @@
 package spring.basic;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBasicRunner {
 
     public static void main(String[] args) {
